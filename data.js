dcc45f21e272d04522d2e753b0dfa603261f4377
johnkhigginson <61078573+johnkhigginson@users.noreply.github.com>
2020-07-27 20:57:07 -0600

    7.27.2020 updates

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 var samplePuzzle = [
     4, 3, 5, 2, 6, 9, 7, 8, 1,
-    6, 8, 2, 8, 7, 1, 4, 9, 3,
+    6, 8, 2, 5, 7, 1, 4, 9, 3,
     1, 9, 7, 8, 3, 4, 5, 6, 2,
     8, 2, 6, 1, 9, 5, 3, 4, 7,
     3, 7, 4, 6, 8, 2, 9, 1, 5,
-    9, 5, 1, 7, 4, 3, 6, 8, 2,
+    9, 5, 1, 7, 4, 3, 6, 2, 8,
     5, 1, 9, 3, 2, 6, 8, 7, 4,
     2, 4, 8, 9, 5, 7, 1, 3, 6,
     7, 6, 3, 4, 1, 8, 2, 5, 9
@@ -38,7 +38,26 @@ var board = [
     "72", "73", "74", "75", "76", "77", "78", "79", "80"
 ];
 
+var grid = [
+  "00", "01", "02", "03", "04", "05", "06", "07", "08",
+  "10", "11", "12", "13", "14", "15", "16", "17", "18",
+  "20", "21", "22", "23", "24", "25", "26", "27", "28",
+  "30", "31", "32", "33", "34", "35", "36", "37", "38",
+  "40", "41", "42", "43", "44", "45", "46", "47", "48",
+  "50", "51", "52", "53", "54", "55", "56", "57", "58",
+  "60", "61", "62", "63", "64", "65", "66", "67", "68",
+  "70", "71", "72", "73", "74", "75", "76", "77", "78",
+  "80", "81", "82", "83", "84", "85", "86", "87", "88"
+];
+
 var boardLength = board.length;
+
+function resetter() {
+  for (var i = 0; i < boardLength; i++) {
+    document.getElementById(board[i]).style.background = "#a0c1d1";
+  }
+
+}
 
 function easy() {
   difficulty = 1;
@@ -54,6 +73,8 @@ function hard() {
 }
 
 function newBoard() {
+  addClick()
+  clicks = 0;
   /*resets cell colors and size if pattern was set*/
   for (i = 0; i < cells.length; i++) {
     cells[i].style.border = "2px solid #b5b2c2";
@@ -74,7 +95,7 @@ function newBoard() {
     temp.push(i);
   }
 
-  j = 24;
+  j = 36;
   /*randomly selects 24 cells to be pre-filled*/
   while (j--) {
     var rand = Math.floor(Math.random() * temp.length);
@@ -93,40 +114,41 @@ function newBoard() {
 
 //Number.isInteger()
 function check() {
+  if (clicks = 0) {
+    var currentBoard = [];
+    var correct = 0;
+    var squares = document.getElementsByClassName("cell");
 
-  var currentBoard = [];
-  var errors = 0;
-  var squares = document.getElementsByClassName("cell");
 
-  for (var i = 0; i < boardLength; i++) {
-    document.getElementById(board[i]).style.background = "#a0c1d1";
-  }
-
-  for (var i = 0; i < boardLength; i++) {
-    var el = document.getElementById(board[i]);
-    var child = el.children[0];
-    if (child.tagName == "INPUT") {
-      currentBoard.push(parseInt(child.value));
+    for (var i = 0; i < boardLength; i++) {
+      var el = document.getElementById(board[i]);
+      var child = el.children[0];
+      if (child.tagName == "INPUT") {
+        currentBoard.push(parseInt(child.value));
+      }
+      else if (child.tagName == "DIV") {
+        currentBoard.push(parseInt(child.textContent));
+      }
     }
-    else if (child.tagName == "DIV") {
-      currentBoard.push(parseInt(child.textContent));
+    for (i = 0; i < samplePuzzle.length; i++) {
+      if (isNaN(currentBoard[i])) {
+        currentBoard[i] = currentBoard[i];
+      }
+      else if (samplePuzzle[i] != currentBoard[i]) {
+        document.getElementById(board[i]).style.background = "#D89A9E";
+      }
+      /*else if (samplePuzzle[i] == currentBoard[i]) {
+        correct = correct + 1;
+      }*/
     }
-  }
-  for (i = 0; i < samplePuzzle.length; i++) {
-    if (samplePuzzle[i] != currentBoard[i]) {
-      document.getElementById(board[i]).style.background = "#D89A9E";
-    }
-    /*else {
-      errors = errors + 1;
+    /*if (correct = 81) {
+      window.alert("You won!");
     }*/
-  }
-  /*if (errors = 81) {
-    document.getElementById("home").click()
-  }*/
-};
-
+    window.setTimeout(resetter, 3000);
+  };
+}
+var cells = document.getElementsByClassName("cell");
 var clicks = 0;
-var cells = document.getElementsByClassName('bigcell');
 
 function pattern() {
   clicks = clicks + 1
@@ -191,6 +213,7 @@ function pattern() {
       }
     }
   }
+  //green diagonal
   if (clicks == 4) {
     var tone1 = ["00", "08", "16", "24", "24", "32", "40", "48", "56", "64", "72", "80"];
     var tone2 = ["07", "15", "23", "31", "39", "47", "55", "63", "73", "65", "57", "49", "41", "33", "25", "17"];
@@ -211,6 +234,7 @@ function pattern() {
       }
     }
   }
+  //blue diagonal
   if (clicks == 5) {
     beg = -1;
     var colors = ["#031238", "#091b4a", "#021b5c", "#052578", "#0637b1", "#1147d0", "#2b58ca", "#5077d9"];
@@ -228,8 +252,28 @@ function pattern() {
   }
 };
 
-var beginCell = '<td class="cell">'
+//Assignment 4
+//cell click function
 
-function inCell(val) {
-  return '<td class="cell" id="' + val + '">\n<input type="number" maxlength="1">\n</td>'
-};
+
+function addClick() {
+  var td = document.getElementsByClassName("cell");
+  for (i = 0; i < td.length; i++) {
+    td[i].onclick = click;
+  }
+}
+/*
+When any cell is clicked, it temporarily changes color and prints
+the row and column location to console.
+*/
+function click() {
+  iden = (grid[board.indexOf(this.id)]).split("");
+  function back() {
+    for (i = 0; i < boardLength; i++) {
+      document.getElementById(board[i]).style.background = "#a0c1d1";
+    }
+  }
+  this.style.background = "#66717E";
+  console.log("Row: " + iden[0] + " Column: " + iden[1]);
+  window.setTimeout(back, 2500);
+}
